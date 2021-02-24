230bd5810473eab82dbb3934ab98beec2e285c77
Rumsbums27 <felix.waidler@web.de>
2021-02-24 18:09:27 +0100

    Improved bot.py and voice.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 from discord.ext import commands
 import json
-from discord import Embed
+from discord import Embed,Intents
 import os
 
-bot = commands.Bot(command_prefix='.')
-bot.remove_command('help')
+bot = commands.Bot(command_prefix='.',intents=Intents.all(),help_command=None)
 
 def get_configs(config):
     with open('configs.json','r') as data:
@@ -13,22 +12,19 @@ def get_configs(config):
 
 @bot.command()
 @commands.is_owner()
-async def load(ctx,extension):
-    bot.load_extension(f'cogs.{extension}')
-
-@bot.command()
-@commands.is_owner()
-async def unload(ctx,extension):
-    bot.unload_extension(f'cogs.{extension}')
-
-@bot.command()
-@commands.is_owner()
 async def reload(ctx,extension):
     bot.unload_extension(f'cogs.{extension}')
     bot.load_extension(f'cogs.{extension}')
+    embed = Embed(
+        title='Bot',
+        description=f'Successfully reloaded {extension}',
+        color=0xa35c3e
+    )
+    await ctx.send(embed=embed)
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
+        print(f'Successfully loaded {filename[:-3]}')
 
 bot.run(get_configs('token'))
